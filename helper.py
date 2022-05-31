dcb2406dd36473c7ddb0c195ee7d0d16325c2b51
Faustineelvaretta <94541551+Faustineelvaretta@users.noreply.github.com>
2022-05-31 14:38:26 +0700

    Add Helper by Faustine

---
+++ helper.py
@@ -0,0 +1,10 @@
+import pygame
+
+GREEN = (0, 255, 0)
+
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font('jupiterc.ttf', size)
+    text_surface = font.render(text, True, GREEN)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
