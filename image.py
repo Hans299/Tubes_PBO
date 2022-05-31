96e7cd54f1e55a85098673040047d8b2795d2012
Hans299 <batubarahans299@gmail.com>
2022-05-31 15:22:08 +0700

    Fixed error

--- image.py
+++ image.py
@@ -1,6 +1,6 @@
 import pygame
 import os
-background=pygame.image.load(os.path.join("image","background.PNG"))
-bullet=pygame.image.load(os.path.join("image","bullet.PNG"))
-player=pygame.image.load(os.path.join("image","player.PNG"))
-rock=pygame.image.load(os.path.join("image","rock.PNG"))
+background=pygame.image.load(os.path.join("image","background.png"))
+bullet=pygame.image.load(os.path.join("image","bullet.png"))
+player=pygame.image.load(os.path.join("image","player.png"))
+rock=pygame.image.load(os.path.join("image","rock.png"))
