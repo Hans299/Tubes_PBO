36a84fb2308b6b611463477f9585d49458c47e9a
Faisal Khairul Fasha <faisal.120140158@student.itera.ac.id>
2022-05-31 14:43:00 +0700

    Add image by Faisal
    menambahkan file image

---
+++ image.py
@@ -0,0 +1,6 @@
+import pygame
+import os
+background=pygame.image.load(os.path.join("image","background.PNG"))
+bullet=pygame.image.load(os.path.join("image","bullet.PNG"))
+player=pygame.image.load(os.path.join("image","player.PNG"))
+rock=pygame.image.load(os.path.join("image","rock.PNG"))
